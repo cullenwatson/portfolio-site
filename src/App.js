32b9b4ae475ec9b1a2980a7383fe5a0ec0230c41
Cullen Watson <cgwatson@smu.edu>
2022-07-23 01:27:26 -0500

    tab name

--- src/App.js
+++ src/App.js
@@ -4,23 +4,19 @@ import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import Skills from "./components/Skills";
 import Work from "./components/Work";
-import {Helmet} from "react-helmet";
+
 
 
 function App() {
   return (
     <div className="App">
-        <Helmet>
-                <meta charSet="utf-8" />
-                <title>Cullen Watson</title>
-                <link rel="canonical" href="http://mysite.com/example" />
-                <meta name="description" content="Cullen Watson's Portfolio Site" />
-        </Helmet>
+        
       <header className="App-header">
       
       </header>
 
-      < Navbar />
+        {/* components */}
+        < Navbar />
         <Home />
         <About />
         <Skills />
