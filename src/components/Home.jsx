1ebf989ebc5dd3147f0148bb512a3d50e6c46e0e
Cullen Watson <cgwatson@smu.edu>
2022-07-22 19:41:41 -0500

    color change

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
         
         {/* Container */}
         <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
-            <p className='text-teal-600'>Hi, my name is</p>
+            <p className='text-[#5ce1e6]'>Hi, my name is</p>
             <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Cullen Watson</h1>
             <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>I'm a CS Student.</h2>
             <p className='text-[#8892b0] py-4 max-w-[700px]'>I'm a junior at SMU studying CS and Finance, with a plan to specialize in Cyber
@@ -18,7 +18,7 @@ const Home = () => {
               <Link  to="about" smooth={true} duration={500}>
             
             
-                <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-teal-600 hover:border-teal-600'>
+                <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:text-[#5ce1e6] hover:text-[#5ce1e6]'>
                   
                   About
                   <span className='group-hover:rotate-90 duration-300'>
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,13 +1,8 @@
 import React from 'react'
-
 import HTML from '../assets/html.png';
 import CSS from '../assets/css.png';
 import JavaScript from '../assets/javascript.png';
-import ReactImg from '../assets/react.png';
-import Node from '../assets/node.png';
-import FireBase from '../assets/firebase.png';
-import AWS from '../assets/aws.png';
-import GitHub from '../assets/github.png';
+
 import Java from '../assets/java.png';
 import Python from '../assets/python.png';
 import CPlus from '../assets/cplus.png';
@@ -18,7 +13,7 @@ const Skills = () => {
         {/* Container */}
         <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
             <div>
-                <p className='text-4xl font-bold inline border-b-4 border-teal-700 '>Skills</p>
+                <p className='text-4xl font-bold inline border-b-4 text-[#5ce1e6] '>Skills</p>
                 <p className='py-4'>// These are the technologies I've worked with</p>
             </div>
             <div className='w-full grid grid-cols-2 sm:grid-cols-3 gap-6 text-center py-8'>
@@ -27,23 +22,23 @@ const Skills = () => {
                     <p className='my-4'>HTML</p>
                 </div>
                 <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                    <img className='w-20 mx-auto' src={CSS} alt="HTML icon"></img>
+                    <img className='w-20 mx-auto' src={CSS} alt="CSS icon"></img>
                     <p className='my-4'>CSS</p>
                 </div>
                 <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                    <img className='w-20 mx-auto' src={JavaScript} alt="HTML icon"></img>
+                    <img className='w-20 mx-auto' src={JavaScript} alt="JAVASCRIPT icon"></img>
                     <p className='my-4'>JAVASCRIPT</p>
                 </div>
                 <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                    <img className='w-20 mx-auto' src={Java} alt="HTML icon"></img>
+                    <img className='w-20 mx-auto' src={Java} alt="JAVA icon"></img>
                     <p className='my-4'>JAVA</p>
                 </div>
                 <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                    <img className='w-20 mx-auto' src={Python} alt="HTML icon"></img>
+                    <img className='w-20 mx-auto' src={Python} alt="PYTHON icon"></img>
                     <p className='my-4'>PYTHON</p>
                 </div>
                 <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                    <img className='w-20 mx-auto' src={CPlus} alt="HTML icon"></img>
+                    <img className='w-20 mx-auto' src={CPlus} alt="C++ icon"></img>
                     <p className='my-4'>C++</p>
                 </div>
                 
